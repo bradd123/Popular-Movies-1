35669cf5dde7796fafffd0c9872de3ac89b7fb16
brahma <brahma@gopassport.com>
2017-08-20 19:48:08 +0530

    wire up all the labels

--- app/src/main/java/com/brahmachilakala/popularmovies1/DetailActivity.java
+++ app/src/main/java/com/brahmachilakala/popularmovies1/DetailActivity.java
@@ -2,8 +2,12 @@ package com.brahmachilakala.popularmovies1;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.squareup.picasso.Picasso;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -13,6 +17,8 @@ public class DetailActivity extends AppCompatActivity {
     private TextView tvMovieDuration;
     private TextView tvMovieRating;
     private TextView btFavorite;
+    private TextView tvMovieOverview;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +31,21 @@ public class DetailActivity extends AppCompatActivity {
         tvMovieDuration = (TextView) findViewById(R.id.tv_movie_duration);
         tvMovieRating = (TextView) findViewById(R.id.tv_movie_rating);
         btFavorite = (TextView) findViewById(R.id.bt_favorite);
+        tvMovieOverview = (TextView) findViewById(R.id.tv_movie_overview);
 
+        tvMovieTitle.setText(getIntent().getStringExtra("original_title"));
+        Picasso.with(this).load(getIntent().getStringExtra("image_url")).into(ivMovieThumbnail);
+        tvMovieOverview.setText(getIntent().getStringExtra("overview"));
+        tvMovieYear.setText(getIntent().getStringExtra("release_date"));
+        tvMovieDuration.setText("Not available");
+        tvMovieRating.setText(getIntent().getStringExtra("user_rating"));
+        btFavorite.setText("Set as Favorite");
+        btFavorite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(DetailActivity.this, "Implement this feature", Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 }
